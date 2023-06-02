85ca727a2afc99d491dea3da17d48cd85b5afd27
Garrett Phillips <gphill1030@gmail.com>
2023-06-02 23:58:43 +0100

    comment block for form clear

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/***********************************************************
+Action that clears contact form after it has been submitted
+************************************************************/
 window.onbeforeunload = () => {
     for(const form of document.getElementsByTagName('form')) {
       form.reset();
