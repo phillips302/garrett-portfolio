33f6aa5b04462cc8a40d4f1086be94bb0b92a3f9
Garrett Phillips <gphill1030@gmail.com>
2023-06-07 00:29:57 +0100

    js again

--- script.js
+++ script.js
@@ -1,65 +1,67 @@
-/***********************************************************
-Utility Function that submits the contact form, then clears
-the contents of the form after waiting 2 second.
-************************************************************/
-function finalizeForm() {
-    var frm = document.getElementById("contactForm");
-    frm.submit(); // Submit the form   
-    setTimeout(function() {
-        frm.reset();  // Reset all form data
-      }, 900);
-    return false; // Prevent page refresh
-}
-
-/***********************************************************
-Background scroll opacity
-************************************************************/
-$(window).scroll(function() {
-  var s = $(window).scrollTop(),
-  opacityVal = (s / 700.0);
-  $('.blurred-img').css('opacity', opacityVal);
-});
-
-/*******************************************
-  Have focus outline only for keyboard users 
-*******************************************/
-
- const handleFirstTab = (e) => {
-  if(e.key === 'Tab') {
-    document.body.classList.add('user-is-tabbing')
-
-    window.removeEventListener('keydown', handleFirstTab)
-    window.addEventListener('mousedown', handleMouseDownOnce)
+$(document).ready(function() {
+  /***********************************************************
+  Utility Function that submits the contact form, then clears
+  the contents of the form after waiting 2 second.
+  ************************************************************/
+  function finalizeForm() {
+      var frm = document.getElementById("contactForm");
+      frm.submit(); // Submit the form   
+      setTimeout(function() {
+          frm.reset();  // Reset all form data
+        }, 900);
+      return false; // Prevent page refresh
   }
 
-}
+  /***********************************************************
+  Background scroll opacity
+  ************************************************************/
+  $(window).scroll(function() {
+    var s = $(window).scrollTop(),
+    opacityVal = (s / 700.0);
+    $('.blurred-img').css('opacity', opacityVal);
+  });
 
-const handleMouseDownOnce = () => {
-  document.body.classList.remove('user-is-tabbing')
+  /*******************************************
+    Have focus outline only for keyboard users 
+  *******************************************/
 
-  window.removeEventListener('mousedown', handleMouseDownOnce)
+  const handleFirstTab = (e) => {
+    if(e.key === 'Tab') {
+      document.body.classList.add('user-is-tabbing')
+
+      window.removeEventListener('keydown', handleFirstTab)
+      window.addEventListener('mousedown', handleMouseDownOnce)
+    }
+
+  }
+
+  const handleMouseDownOnce = () => {
+    document.body.classList.remove('user-is-tabbing')
+
+    window.removeEventListener('mousedown', handleMouseDownOnce)
+    window.addEventListener('keydown', handleFirstTab)
+  }
+
   window.addEventListener('keydown', handleFirstTab)
-}
 
-window.addEventListener('keydown', handleFirstTab)
+  const backToTopButton = document.querySelector(".back-to-top");
+  let isBackToTopRendered = false;
 
-const backToTopButton = document.querySelector(".back-to-top");
-let isBackToTopRendered = false;
+  let alterStyles = (isBackToTopRendered) => {
+    backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
+    backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
+    backToTopButton.style.transform = isBackToTopRendered
+      ? "scale(1)"
+      : "scale(0)";
+  };
 
-let alterStyles = (isBackToTopRendered) => {
-  backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
-  backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
-  backToTopButton.style.transform = isBackToTopRendered
-    ? "scale(1)"
-    : "scale(0)";
-};
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 700) {
-    isBackToTopRendered = true;
-    alterStyles(isBackToTopRendered);
-  } else {
-    isBackToTopRendered = false;
-    alterStyles(isBackToTopRendered);
-  }
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 700) {
+      isBackToTopRendered = true;
+      alterStyles(isBackToTopRendered);
+    } else {
+      isBackToTopRendered = false;
+      alterStyles(isBackToTopRendered);
+    }
+  });
 });
