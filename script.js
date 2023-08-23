25ebeccbd095abcfb41cf46689d180efa9affb0e
Garrett Phillips <gphill1030@gmail.com>
2023-08-22 22:27:36 -0400

    fixed js issue where come code wasn't executing

--- script.js
+++ script.js
@@ -20,29 +20,6 @@ $(window).scroll(function() {
   $('.blurred-img').css('opacity', opacityVal);
 });
 
-/***********************************************************
-Image Slideshow
-************************************************************/
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slide");
-
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-
-  slides[slideIndex-1].style.display = "block";
-}
-
 $(function() {
 
   /*******************************************
@@ -118,3 +95,26 @@ $(function() {
     readMoreFooterText.classList.toggle("show_more_footer");
   });
 });
+
+/***********************************************************
+Image Slideshow
+************************************************************/
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "block";
+}
