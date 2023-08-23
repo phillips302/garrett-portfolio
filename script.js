8caaa21a5033ce5de638b0d1e82c699678da7819
Garrett Phillips <gphill1030@gmail.com>
2023-08-22 22:26:03 -0400

    Added slideshow feature to projects tab

--- script.js
+++ script.js
@@ -20,6 +20,29 @@ $(window).scroll(function() {
   $('.blurred-img').css('opacity', opacityVal);
 });
 
+/***********************************************************
+Image Slideshow
+************************************************************/
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "block";
+}
+
 $(function() {
 
   /*******************************************
